70c037de20a80a11d9cecc915fefaa6bc6f81034
BinduraUniversitySoftwareHub <41635871+BinduraUniversitySoftwareHub@users.noreply.github.com>
2018-07-26 16:35:24 +0200

    Dashboard
    Tracker, inbox and Outbox

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,16 +17,76 @@
                     <h3>Welcome to Mailbox</h3>
                     <div class="col-md-12">
                         <div class="col-md-4">
-                            <img src="img/inbox.png" width="40%" height="20%">
+                                <button type="button" class="btn btn-default btn-sm " data-toggle="modal" data-target="#inbox" style="border:0px solid transparent;">
+                                    <img src="img/inbox.png" width="40%" height="20%">
+                                </button>
                         </div>
                         <div class="col-md-4">
-                            <img src="img/outbox.png" width="40%" height="20%">
+                                <button type="button" class="btn btn-default btn-sm " data-toggle="modal" data-target="#outbox" style="border:0px solid transparent;">
+                                    <img src="img/outbox.png" width="40%" height="20%">
+                                </button>
                         </div>
                         <div class="col-md-4">
-                            <img src="img/tracker.png" width="40%" height="20%"40>
+                                <button type="button" class="btn btn-default btn-sm " data-toggle="modal" data-target="#tracker" style="border:0px solid transparent;">
+                                         <img src="img/tracker.png" width="40%" height="20%">
+                                </button>
                         </div>
                     </div>
                     </center>
+                </div>
+                <!-- Modal For Inbox -->
+                <div class="modal fade" id="inbox" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                  <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title" id="myModalLabel">Inbox</h4>
+                      </div>
+                      <div class="modal-body">
+                        ...
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                 <!-- Modal For Outbox -->
+                <div class="modal fade" id="outbox" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                  <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title" id="myModalLabel">Outbox</h4>
+                      </div>
+                      <div class="modal-body">
+                        ...
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                 <!-- Modal For Tracker -->
+                <div class="modal fade" id="tracker" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                  <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title" id="myModalLabel">Tracker</h4>
+                      </div>
+                      <div class="modal-body">
+                        ...
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                      </div>
+                    </div>
+                  </div>
                 </div>
             </div>
         </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -72,6 +72,7 @@
         </nav>
 
         @yield('content')
+        @include('layouts.inbox')
     </div>
 
     <!-- Scripts -->
