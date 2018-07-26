f6e3f0fcb0ea427b1125db7f841a8a64cc7a7b1f
musa <tmmusariri@hotmail.com>
2018-07-26 10:09:36 +0200

    final

---
+++ database/migrations/2018_07_26_063445_create_mails_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mails', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('subject');
+            $table->integer('from');
+            $table->integer('to');
+            $table->integer('userId')->unsigned();
+            $table->timestamps();
+            $table->foreign('userId')->references('id')->on('users')->onDelete('restrict')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mails');
+    }
+}
---
+++ database/migrations/2018_07_26_063621_create_trackers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTrackersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trackers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('departmentId')->unsigned();
+            $table->integer('mailId')->unsigned();
+            $table->timestamps();
+            $table->foreign('mailId')->references('id')->on('mails')->onDelete('restrict')->onUpdate('cascade');
+            $table->foreign('departmentId')->references('id')->on('departments')->onDelete('restrict')->onUpdate('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trackers');
+    }
+}
